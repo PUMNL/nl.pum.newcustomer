e5bed208cb0b84c30b808ed34b37aba0501e654b
Jaap Jansma <info@edeveloper.nl>
2014-08-16 16:01:20 +0200

    fixed problem with config class

--- CRM/Newcustomer/Config.php
+++ CRM/Newcustomer/Config.php
@@ -8,6 +8,10 @@ class CRM_Newcustomer_Config {
   
   private $drupal_role = false;
   
+  private $auth_for_relationship;
+  
+  private $representative_relationship;
+  
   private function __construct() {
     $this->relationship_types = unserialize(CRM_Core_BAO_Setting::getItem('nl.pum.newcustomer', 'new_customer_relationship_types'));
     if (!is_array($this->relationship_types)) {
@@ -19,6 +23,9 @@ class CRM_Newcustomer_Config {
     if (isset($roles[$rid])) {
       $this->drupal_role = $rid;
     }
+    
+    $this->auth_for_relationship = civicrm_api3('RelationshipType', 'getsingle', array('name_a_b' => 'Has authorised'));
+    $this->representative_relationship = civicrm_api3('RelationshipType', 'getsingle', array('name_a_b' => 'Representative'));
   }
   
   public static function singleton() {
@@ -42,6 +49,20 @@ class CRM_Newcustomer_Config {
       $roles = user_roles(true);
     }
     return $roles;
+  }
+  
+  public function getAuthorizedContactRelationshipTypeId() {
+    if (isset($this->auth_for_relationship['id'])) {
+      return $this->auth_for_relationship['id'];
+    }
+    return false;
+  }
+  
+  public function getRepresepentativeRelationshipTypeId() {
+    if (isset($this->representative_relationship['id'])) {
+      return $this->representative_relationship['id'];
+    }
+    return false;
   }
   
 }
